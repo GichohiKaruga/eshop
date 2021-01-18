5f23bab301aed69cc8c34a892122b9a309f7a9ba
GichohiKaruga <gichohi.karuga@outlook.com>
2021-01-18 14:41:03 +0300

    Docker files update

--- products/spec/requests/products_request_spec.rb
+++ products/spec/requests/products_request_spec.rb
@@ -13,17 +13,6 @@ describe 'Products API', type: :request do
     end
   end
 
-  describe 'POST /products' do
-    it 'create a new product' do
-      expect do
-        post '/api/v1/products', params: {
-          product: { name: 'Xiaomi Redmi Note 9', description: 'Premiering processor.', price: 229.95 }
-        }
-      end.to change { Product.count }.from(0).to(1)
-      expect(response).to have_http_status(:created)
-    end
-  end
-
   describe 'GET a product by id' do
     let!(:product) { FactoryBot.create(:product) }
     it 'return a product' do
